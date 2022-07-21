913aec7bf8473324087ffe82cd27b2bd4df958f6
Adam Birse <adambirse@gmail.com>
2022-07-21 08:29:28 +0100

    S3 event handler persists file contents to dynamo db

--- src/functions/S3EventHandler/handler.ts
+++ src/functions/S3EventHandler/handler.ts
@@ -1,11 +1,19 @@
 import { middyfy } from "@libs/lambda";
 import S3 from "aws-sdk/clients/s3";
+import DynamoDB from "aws-sdk/clients/dynamodb";
 
 import { S3Event } from "aws-lambda";
 import * as csv from "@fast-csv/parse";
 import { Readable } from "stream";
 
 const s3: S3 = new S3({ apiVersion: "2006-03-01", region: "eu-west-1" });
+const ddb = new DynamoDB({ apiVersion: "2012-08-10" });
+
+interface Item {
+  key: string;
+  value1: string;
+  value2: string;
+}
 
 const s3EventHandler = async (event: S3Event) => {
   for (const record of event.Records) {
@@ -36,8 +44,6 @@ const handle = async (record) => {
   }
 };
 
-export const main = middyfy(s3EventHandler);
-
 const parseCSV = async (csvStream: Readable) => {
   return new Promise((resolve, reject) => {
     const parsedData = [];
@@ -53,7 +59,7 @@ const parseCSV = async (csvStream: Readable) => {
       })
       .on("end", async () => {
         if (parsedData.length > 0) {
-          persist(parsedData);
+          await persist(parsedData);
         } else {
           console.log("No parsed data to upload");
         }
@@ -61,8 +67,38 @@ const parseCSV = async (csvStream: Readable) => {
       });
   });
 };
-const persist = (data: any[]) => {
-  data.forEach((item) => {
-    console.log(item);
-  });
+const persist = async (data: any[]) => {
+  try {
+    for (const item of data) {
+      await insertData(convert(item));
+    }
+  } catch (e) {
+    console.log(e);
+  }
 };
+
+const insertData = async (item: Item) => {
+  console.log(`Inserting ${JSON.stringify(item)} in DB`);
+  const putParams = {
+    TableName: process.env.RESOURCE_TABLE,
+    Item: {
+      id: { S: item.key },
+      ID1: { S: item.value1 },
+      ID2: { S: item.value2 },
+    },
+  };
+
+  await ddb.putItem(putParams).promise();
+};
+
+const convert = (item: any): Item => {
+  var result = {
+    key: item[0],
+    value1: item[1],
+    value2: item[2],
+  };
+
+  return result;
+};
+
+export const main = middyfy(s3EventHandler);
